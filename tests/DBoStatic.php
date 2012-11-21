0dc3a462d955daa036c3fed157fe6a10921ee5a8
Thomas Bley <thbley+github@gmail.com>
2012-11-21 02:30:50 +0100

    added begin, commit tests

--- tests/DBoStatic.php
+++ tests/DBoStatic.php
@@ -2,9 +2,29 @@
 
 require "DBo.php";
 
+class mysqli_log extends mysqli {
+	public static $queries = [];
+	
+	public function query($query) {
+		self::$queries[] = $query;
+		return parent::query($query);
+	}
+}
+
 class DBoStatic extends PHPUnit_Framework_TestCase {
 
-	public function testDummy() {
-        $this->assertEquals(2, 2);
+	protected function setUp() {
+		mysqli_log::$queries = [];
+		DBo::conn(new mysqli_log("127.0.0.1", "root", "", "test"));
+    }
+
+	public function testBegin() {
+        DBo::begin();
+		$this->assertEquals(mysqli_log::$queries, ["begin"]);
+    }
+
+	public function testCommit() {
+        DBo::commit();
+		$this->assertEquals(mysqli_log::$queries, ["commit"]);
     }
 }
