3ec9e27124b7fd87c853c10d3bbe35d88ea73d8c
Thomas Bley <thbley+github@gmail.com>
2012-12-17 00:30:26 +0100

    cleanup

--- tests/DBoObjectTest.php
+++ tests/DBoObjectTest.php
@@ -8,17 +8,10 @@ require_once "DBo.php";
 class DBoObjectTest extends PHPUnit_Framework_TestCase {
 
 	public function testCall() {
-	/* TODO test
-		$dbo = new DBo("hello");
-		$this->assertAttributeEquals([["table"=>"hello", "params"=>null]], "stack", $dbo);
-	*/
-
 		$dbo = new DBo("hello", "world");
 		$this->assertAttributeEquals([["table"=>"hello", "params"=>"world"]], "stack", $dbo);
 
-	/* TODO test
 		$dbo = (new DBo("hello"))->world();
 		$this->assertAttributeEquals([["table"=>"world", "params"=>null], ["table"=>"hello", "params"=>null]], "stack", $dbo);
-	*/
 	}
 }
