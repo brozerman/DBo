9cebb8ef270b24af980a8e5b68571740704104d9
Thomas Bley <thbley+github@gmail.com>
2012-12-17 00:15:38 +0100

    cleanup

--- tests/DBoObjectTest.php
+++ tests/DBoObjectTest.php
@@ -2,24 +2,10 @@
 
 require_once "DBo.php";
 
-class mysqli_log extends mysqli {
-	public static $queries = [];
-	
-	public function query($query) {
-		self::$queries[] = $query;
-		return parent::query($query);
-	}
-}
-
 /**
  * DBo test object methods
  */
 class DBoObjectTest extends PHPUnit_Framework_TestCase {
-
-	protected function setUp() {
-		mysqli_log::$queries = [];
-		DBo::conn(new mysqli_log("127.0.0.1", "root", "", "test"));
-	}
 
 	public function testCall() {
 	/* TODO test
