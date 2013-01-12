844daf2aa9ede8c68a8a18b6e4257a5069d3faf4
Thomas Bley <thbley+github@gmail.com>
2013-01-12 19:36:59 +0100

    cleanup

--- tests/DBoStaticTest.php
+++ tests/DBoStaticTest.php
@@ -320,7 +320,7 @@ class DBoStaticTest extends PHPUnit_Framework_TestCase {
 			$this->assertInstanceOf("DBo", $o);
 			$this->assertEquals($o->a, '1');
 		}
-		foreach (DBo::object("SELECT b FROM test.t1 WHERE a=?", [1]) as $o) {
+		foreach (DBo::object("SELECT c FROM test.t1 WHERE a=?", [1]) as $o) {
 			$this->assertInstanceOf("DBo", $o);
 			$this->assertEquals($o->c, "ab");
 		}
